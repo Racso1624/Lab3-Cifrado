6861a1e719ab005eb7601d857baed16b30629271
Racso1624 <fer10_lb@outlook.com>
2024-03-06 18:51:07 -0600

    Se termina parte 1

--- functions.py
+++ functions.py
@@ -13,3 +13,8 @@ def decrypt_aes_ecb(cypher_data, key):
     cipher = AES.new(key, AES.MODE_ECB)
     decrypt_data = unpad(cipher.decrypt(cypher_data), AES.block_size)
     return decrypt_data
+
+def decrypt_aes_cbc(cypher_data, key):
+    cipher = AES.new(key, AES.MODE_CBC)
+    decrypt_data = unpad(cipher.decrypt(cypher_data), AES.block_size)
+    return decrypt_data
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from functions import *
 import hashlib
 
+# Parte 1
+
 key_1 = load_file('./data/mr-increible.key')
 key_1 = key_1.decode('utf-8')
 image_1 = load_file('./data/mr-increible_encrypted_image.jpeg')
 decrypted_image_1 = decrypt_aes_ecb(image_1, bytes.fromhex(key_1))
 save_file('./results/mr-increible-decrypted.jpeg', decrypted_image_1)
+
+key_2 = load_file('./data/ayno.key')
+key_2 = key_2.decode('utf-8')
+image_2 = load_file('./data/ayno_encrypted_image.jpeg')
+decrypted_image_2 = decrypt_aes_cbc(image_2, bytes.fromhex(key_2))
+save_file('./results/ayno-decrypted.jpeg', decrypted_image_2)
